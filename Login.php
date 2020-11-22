676ae25253998093b4a048151a9bd84590dc891a
choi-jaewon <31034469+choi-jaewon@users.noreply.github.com>
2020-11-22 14:34:31 +0900

    Update Login.php

--- Login.php
+++ Login.php
@@ -2,22 +2,22 @@
     $con = mysqli_connect("localhost", "moapp", "wodnjs13!", "moapp");
     mysqli_query($con,'SET NAMES utf8');
 
-    $usr_num = $_POST["usr_num"];
+    $id = $_POST["id"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE usr_num = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $usr_num, $password);
+    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE id = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $id, $password);
     mysqli_stmt_execute($statement);
 
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $usr_num, $password, $usr_name);
+    mysqli_stmt_bind_result($statement, $id, $password, $usr_name);
 
     $response = array();
     $response["success"] = false;
  
     while(mysqli_stmt_fetch($statement)) {
         $response["success"] = true;
-        $response["usr_num"] = $user_num;
+        $response["id"] = $id;
         $response["password"] = $password;
         $response["usr_name"] = $usr_name;     
     }
